d17759ad679be91f2dd26dd872c8b68bc2e4e376
Eric Wilson <eawww0@gmail.com>
2019-06-03 09:31:37 -0500

    Refactor MacLeod component to simplify 🍖🥔🥔 of example

--- meet-hooks/src/Examples/extras/MacLeod.js
+++ meet-hooks/src/Examples/extras/MacLeod.js
@@ -2,17 +2,19 @@ import React, {useEffect, useRef} from 'react';
 import '../../App.css';
 import ScowlingImmortal from './MacLeod.png'
 
+const appendHighlander = (ref) => {
+  const newImg = document.createElement('img')
+  newImg.setAttribute('class', 'MacLeod')
+  newImg.setAttribute('src', ScowlingImmortal)
+  newImg.setAttribute('style', `left: ${(Math.random() - 0.05) * window.innerWidth}px;`)
+  ref.current.appendChild(newImg);
+}
+
 const MacLeod = ({qty}) => {
   const highlanderHolderRef = useRef()
-  const appendHighlander = () => {
-    const newImg = document.createElement('img')
-    newImg.setAttribute('class', 'MacLeod')
-    newImg.setAttribute('src', ScowlingImmortal)
-    newImg.setAttribute('style', `left: ${(Math.random() - 0.05) * window.innerWidth}px;`)
-    highlanderHolderRef.current.appendChild(newImg);
-  }
+  
   useEffect(() => {
-    appendHighlander()
+    appendHighlander(highlanderHolderRef)
   })
   return <div
     id="highlanderHolder"
